a1dad6a25702e660ce26c947fe39b88e1359ceec
GinaMaddox <gina.maddox@gmail.com>
2018-07-02 22:42:06 -0400

    new commit

---
+++ app/controllers/client/products_controller.rb
@@ -0,0 +1,17 @@
+class Client::ProductsController < ApplicationController
+  def index
+    response = Unirest.get("http://localhost:3000/api/products")
+    @products = response.body
+    render "index.html.erb"
+  end  
+
+
+  def show
+    product_id = params[:id]
+    response = Unirest.get("http://localhost:3000/api/products#{product_id}")
+    @products = response.body
+    render "show.html.erb"
+  end
+
+  def new 
+end
