6db3b0a7a6a89ba4ce9e92d786da8a657f5af532
GinaMaddox <gina.maddox@gmail.com>
2018-07-04 13:19:29 -0400

    add update, delete, and links

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -1,7 +1,7 @@
 class Client::ProductsController < ApplicationController
   def index
     response = Unirest.get("http://localhost:3000/api/products")
-    @products = response.body
+    @product = response.body
     render "index.html.erb"
   end  
 
@@ -9,9 +9,50 @@ class Client::ProductsController < ApplicationController
   def show
     product_id = params[:id]
     response = Unirest.get("http://localhost:3000/api/products#{product_id}")
-    @products = response.body
+    @product = response.body
     render "show.html.erb"
   end
 
-  # def new 
+  def new 
+    render "new.html.erb"
+  end
+
+  def create
+    response = Unirest.post("http://localhost:3000/api/products", parameters:
+     {
+      name: params[:input_name],
+      price: params[:input_price],
+      image_url: params[:input_image_url],
+      description: params[:input_description]
+    }
+   )
+  @product = response.body
+  render "show.html.erb"
+end
+
+  def edit
+    product_id = params[:id]
+    response = Unirest.get("http://localhost:3000/api/products#{product_id}")
+    @product = response.body
+    render "edit.html.erb"
+ end
+
+ def update
+  product_id = params[:id]
+  client_params = {
+    name: params[:input_name],
+    price: params[:input_price],
+    image_url: params[:input_image_url],
+    description: params[:input_description]
+  }
+  response = Unirest.patch("http://localhost:3000/api/products#{product_id}", parameters: client_params)
+  @product = response.body
+  render "show.html.erb"
+ end
+
+def destroy
+  product_id = params[:id]
+  response = Unirest.delete("localhost:3000/api/products#{product_id}")
+  render "destroy.html.erb"
+end
 end
